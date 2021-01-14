59c2bc4c14b375b78bd4ecbdf36dd7055eff9358
David de Castro Hernandez <itsdavidch@gmail.com>
2021-01-14 21:07:52 +0100

    Created EntityDtoConverter class

--- src/main/java/com/dtw/demoSpringBoot/service/PersonService.java
+++ src/main/java/com/dtw/demoSpringBoot/service/PersonService.java
@@ -1,8 +1,6 @@
 package com.dtw.demoSpringBoot.service;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +8,7 @@ import com.dtw.demoSpringBoot.dto.PersonDto;
 import com.dtw.demoSpringBoot.entity.Person;
 import com.dtw.demoSpringBoot.exceptions.PersonNotFoundException;
 import com.dtw.demoSpringBoot.repo.PersonRepo;
+import com.dtw.demoSpringBoot.utils.EntityDtoConverter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.MapperFeature;
@@ -18,7 +17,7 @@ import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.JsonPatchException;
 
 @Service
-public class PersonService {
+public class PersonService extends EntityDtoConverter<Person, PersonDto>{
 
 	@Autowired
 	private PersonRepo personRepo;
@@ -26,11 +25,13 @@ public class PersonService {
 	@Autowired
 	private ObjectMapper objectMapper;
 	
-	@Autowired
-	private ModelMapper modelMapper;
+	
+	public PersonService() {
+		super(Person.class, PersonDto.class);
+	}
 	
 	
-	public List<Person> getAll() {
+	public List<Person> getAll() {		
 		return personRepo.findAll();
 	}
 	
@@ -54,33 +55,5 @@ public class PersonService {
 		//TODO: validate patched object like you would with @Valid
 		
 		return toEntity(objectMapper.disable(MapperFeature.USE_ANNOTATIONS).treeToValue(patched, PersonDto.class));
-	}
-	
-	public PersonDto toDto(Person person) {
-		return modelMapper.map(person, PersonDto.class);
-	}
-	
-	public Person toEntity(PersonDto personDto) {
-		return modelMapper.map(personDto, Person.class);
-	}
-	
-	public List<PersonDto> listToDto(List<Person> personList) {
-		List<PersonDto> personDtoList = new ArrayList<PersonDto>();
-		
-		for(Person person : personList) {
-			personDtoList.add(toDto(person));
-		}
-		
-		return personDtoList;
-	}
-	
-	public List<Person> listToEntity(List<PersonDto> personDtoList) {
-		List<Person> personList = new ArrayList<Person>();
-		
-		for(PersonDto personDto : personDtoList) {
-			personList.add(toEntity(personDto));
-		}
-		
-		return personList;
 	}
 }
