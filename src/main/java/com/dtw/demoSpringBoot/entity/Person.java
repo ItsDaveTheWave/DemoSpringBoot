7a8f7f6ab375cbf5732e89b42b303bee70e7969a
David de Castro Hernandez <itsdavidch@gmail.com>
2021-02-02 08:39:54 +0100

    Added constraints to entity

--- src/main/java/com/dtw/demoSpringBoot/entity/Person.java
+++ src/main/java/com/dtw/demoSpringBoot/entity/Person.java
@@ -7,6 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,9 +28,12 @@ public class Person {
 	private Long id;
 	
 	@Column(nullable = false)
+	@NotBlank
 	private String name;
 	
 	@Column(nullable = false)
+	@NotNull
+	@Positive
 	private Integer age;
 
 	@ElementCollection
