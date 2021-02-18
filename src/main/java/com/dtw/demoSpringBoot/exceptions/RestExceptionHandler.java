467599b3f6a2dfe6ad9dd339092533b93d7800f5
David de Castro Hernandez <itsdavidch@gmail.com>
2021-02-18 21:16:15 +0100

    Eliminated some unnecesary exception handlers. Extended the exception handler class to ResponseEntityExceptionHandler, a default handler for exceptions that handles some of them for us

--- src/main/java/com/dtw/demoSpringBoot/exceptions/RestExceptionHandler.java
+++ src/main/java/com/dtw/demoSpringBoot/exceptions/RestExceptionHandler.java
@@ -5,56 +5,51 @@ import javax.validation.ConstraintViolationException;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.transaction.TransactionSystemException;
 import org.springframework.validation.FieldError;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.fge.jsonpatch.JsonPatchException;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
 	// Generic error
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<ApiError> handleException(Exception ex) {
 		ex.printStackTrace();
 		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error", ex);
-		return buildResponseEntity(apiError);
+		return buildResponseEntityApiError(apiError);
 	}
-
-	// Invalid path/method error
-	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-	protected ResponseEntity<ApiError> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
-		ApiError apiError = new ApiError(HttpStatus.METHOD_NOT_ALLOWED, "Method not allowed on this path", ex);
-		return buildResponseEntity(apiError);
-	}
-
+	
 	// Entity not found error
 	@ExceptionHandler(EntityNotFoundException.class)
 	protected ResponseEntity<ApiError> handleEntityNotFound(EntityNotFoundException ex) {
 		ApiError apiError = new ApiError(HttpStatus.NOT_FOUND);
 		apiError.setMessage(ex.getMessage());
-		return buildResponseEntity(apiError);
+		return buildResponseEntityApiError(apiError);
 	}
 
-	// Validation error by @Valid
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	protected ResponseEntity<ApiError> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+	// Validation error by @Valid	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST);
 		apiError.setMessage("Validation error");
 		for (FieldError error : ex.getFieldErrors()) {
 			apiError.addSubError(new ApiValidationError(ex.getTarget().getClass(), error));
 		}
-		return buildResponseEntity(apiError);
+		return new ResponseEntity<> (apiError, apiError.getStatus());
 	}
 	
 	// Can be thrown when commiting changes to DB, can contain ConstraintViolationException(if error is because of validation)
@@ -76,17 +71,17 @@ public class RestExceptionHandler {
 		for(ConstraintViolation<?> violation : ex.getConstraintViolations()) {
 			apiError.addSubError(new ApiValidationError(violation));
 		}
-		return buildResponseEntity(apiError);
+		return buildResponseEntityApiError(apiError);
 	}
 
 	// Json parse error
-	@ExceptionHandler({ HttpMessageNotReadableException.class, JsonProcessingException.class })
-	protected ResponseEntity<ApiError> handleHttpMessageNotReadable_JsonProcessing(Exception ex) {
+	@ExceptionHandler(JsonProcessingException.class)
+	protected ResponseEntity<ApiError> handleJsonProcessing(Exception ex) {
 		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "JSON processing/parse error", ex);
-		return buildResponseEntity(apiError);
+		return buildResponseEntityApiError(apiError);
 	}
 
-	// Type conversion error
+	// Type conversion error on controller method argument
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
 	protected ResponseEntity<ApiError> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
 		String message = "Type conversion error.";
@@ -99,17 +94,17 @@ public class RestExceptionHandler {
 		}
 		
 		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, message, ex);
-		return buildResponseEntity(apiError);
+		return buildResponseEntityApiError(apiError);
 	}
 
 	// JsonPatch error
 	@ExceptionHandler(JsonPatchException.class)
 	protected ResponseEntity<ApiError> handleJsonPatch(JsonPatchException ex) {
 		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "JSONPatch error.", ex);
-		return buildResponseEntity(apiError);
+		return buildResponseEntityApiError(apiError);
 	}
 
-	private ResponseEntity<ApiError> buildResponseEntity(ApiError apiError) {
+	private ResponseEntity<ApiError> buildResponseEntityApiError(ApiError apiError) {
 		return new ResponseEntity<>(apiError, apiError.getStatus());
 	}
 }
