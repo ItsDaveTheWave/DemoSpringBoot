8007bf3f8d02764eafb34e7f9a82bd94237498bd
David de Castro Hernandez <itsdavidch@gmail.com>
2021-04-21 09:49:34 +0200

    Added constraints to controller interface

--- src/main/java/com/dtw/demoSpringBoot/controller/IController.java
+++ src/main/java/com/dtw/demoSpringBoot/controller/IController.java
@@ -2,6 +2,9 @@ package com.dtw.demoSpringBoot.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
 import org.springframework.http.ResponseEntity;
 
 import com.dtw.demoSpringBoot.exceptions.EntityNotFoundException;
@@ -11,10 +14,10 @@ import com.github.fge.jsonpatch.JsonPatchException;
 
 public interface IController<Entity> {
 
-	ResponseEntity<List<Entity>> getAll(Integer page, Integer pageSize, String sortBy);
+	ResponseEntity<List<Entity>> getAll(@Min(0) Integer page, @Min(1) Integer pageSize, String sortBy);
 	ResponseEntity<Entity> getOne(Long id) throws EntityNotFoundException;
-	ResponseEntity<Entity> create(Entity entity);
-	ResponseEntity<Entity> totalUpdate(Long id, Entity entity) throws EntityNotFoundException;
+	ResponseEntity<Entity> create(@Valid Entity entity);
+	ResponseEntity<Entity> totalUpdate(Long id, @Valid Entity entity) throws EntityNotFoundException;
 	ResponseEntity<Entity> partialUpdate(Long id, JsonNode mergePatchNode) 
 			throws EntityNotFoundException, JsonProcessingException, JsonPatchException, IllegalArgumentException, IllegalAccessException;
 	ResponseEntity<Void> delete(Long id) throws EntityNotFoundException;
